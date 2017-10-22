75791a0f72b9138d5b27ae49cf5f084a2e3a4f85
kdrbyrz <kadirboyraz78@gmail.com>
2017-10-22 16:54:23 +0300

    Get Ram Data
    Get and push ram data to index.html

--- monitor/views.py
+++ monitor/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-
+import requests
 # Create your views here.
 def index(request):
-    values= [['foo', 32], ['bar', 64], ['baz', 96]]
+    values= []
+    r = requests.get('http://localhost:8080/data')
+    res=r.json()
+    for key, value in res.items():
+    	values.append(['total ram',value[0][1]])
+    	values.append(['used ram',value[1][1]])
+    	#values.append(['Disk',value[2][1]])
     return render(request, 'monitor/index.html', {'values': values})
 
-def data(request):
-
-    return render(request, 'monitor/index.html',{'values':values})
--- server.py
+++ server.py
@@ -2,19 +2,19 @@ from bottle import run, template, get, post, request
 from pysnmp.entity.rfc3413.oneliner import cmdgen
 
 cmdGen = cmdgen.CommandGenerator()
-data=[]
 # server url (example: host:post/data)
 @get('/data')
-
 def oidtoSend():
+    data=[]
     # grab data from form
+
     errorIndication, errorStatus, errorIndex, varBindTable = cmdGen.nextCmd(
         cmdgen.CommunityData('public'),
         cmdgen.UdpTransportTarget(('localhost', 161)),
-        '1.3.6.1.4.1.2021.10.1.3.1', #CPU 1 minute load
+        '1.3.6.1.4.1.2021.4.5', #Total Ram
         # '1.3.6.1.2.1.1.1',         #System Description
         '1.3.6.1.4.1.2021.4.6',      #total RAM used
-        '1.3.6.1.4.1.2021.9.1.8.1',  #Path where the disk is mounted
+      #  '1.3.6.1.4.1.2021.9.1.8.1',  #Path where the disk is mounted
     )
     if errorIndication:
         print(errorIndication)
@@ -28,9 +28,11 @@ def oidtoSend():
         else:
             for varBindTableRow in varBindTable:
                 for name, val in varBindTableRow:
-                    data.append(val.prettyPrint())
+                    list1=[name.prettyPrint(),val.prettyPrint()]
+                    data.append(list1)
+    return {'response': data}
     print (data)
-    return data
+    
 
 # make api call
 run(host='localhost', port=8080)
