1cf50bc99a68452fd4816ab52aab67c917d6c9d4
comnurz <slbandidas@gmail.com>
2017-10-22 18:25:13 +0300

    Created Urls
    Created urls

--- server.py
+++ server.py
@@ -2,19 +2,13 @@ from bottle import run, template, get, post, request
 from pysnmp.entity.rfc3413.oneliner import cmdgen
 
 cmdGen = cmdgen.CommandGenerator()
-# server url (example: host:post/data)
-@get('/data')
-def oidtoSend():
-    data=[]
-    # grab data from form
 
+def oid(oids):
+    data=[]
     errorIndication, errorStatus, errorIndex, varBindTable = cmdGen.nextCmd(
         cmdgen.CommunityData('public'),
         cmdgen.UdpTransportTarget(('localhost', 161)),
-        '1.3.6.1.4.1.2021.4.5', #Total Ram
-        # '1.3.6.1.2.1.1.1',         #System Description
-        '1.3.6.1.4.1.2021.4.6',      #total RAM used
-      #  '1.3.6.1.4.1.2021.9.1.8.1',  #Path where the disk is mounted
+        *oids,
     )
     if errorIndication:
         print(errorIndication)
@@ -30,9 +24,30 @@ def oidtoSend():
                 for name, val in varBindTableRow:
                     list1=[name.prettyPrint(),val.prettyPrint()]
                     data.append(list1)
+    return data
+
+# server url (example: host:post/data)
+@get('/ramdata')
+def ram():
+    oids=['1.3.6.1.4.1.2021.4.5','1.3.6.1.4.1.2021.4.6']
+#               total Ram          ,    total ram used
+    data=oid(oids)
     return {'response': data}
-    print (data)
-    
+
+
+@get('/cpudata')
+def cpu():
+    oids=['1.3.6.1.4.1.2021.10.1.3.1','1.3.6.1.4.1.2021.10.1.3.2']
+#           1 minute load               5 minute load
+    data=oid(oids)
+    return {'response': data}
+
+@get('/diskdata')
+def disk():
+    oids=['1.3.6.1.4.1.2021.9.1.8.1','1.3.6.1.4.1.2021.9.1.7.1']
+#           used                        avaible
+    data=oid(oids)
+    return {'response': data}
 
 # make api call
 run(host='localhost', port=8080)
