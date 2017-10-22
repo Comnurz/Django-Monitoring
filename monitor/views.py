4f9b5e8a8be6bedcc166b750c765e398dea53642
kdrbyrz <kadirboyraz78@gmail.com>
2017-10-22 18:37:22 +0300

    Get Data and Show
    Get data from server and make it chart

--- monitor/views.py
+++ monitor/views.py
@@ -1,13 +1,33 @@
 from django.shortcuts import render
 import requests
 # Create your views here.
-def index(request):
-    values= []
-    r = requests.get('http://localhost:8080/data')
-    res=r.json()
-    for key, value in res.items():
-    	values.append(['total ram',value[0][1]])
-    	values.append(['used ram',value[1][1]])
-    	#values.append(['Disk',value[2][1]])
-    return render(request, 'monitor/index.html', {'values': values})
-
+def index(request):  
+  ramValues,diskValues,cpuValues=requestData() 
+  return render(request, 'monitor/index.html', { 
+    'ramValues': ramValues, 
+    'diskValues':diskValues,
+    'cpuValues':cpuValues 
+ 
+    }) 
+ 
+def requestData(): 
+  cpuList= [] 
+  ramList=[] 
+  diskList=[['a',123]] 
+  reqRam = requests.get('http://localhost:8080/ramdata') 
+  ramList=listBuilder(reqRam) 
+  reqCpu = requests.get('http://localhost:8080/cpudata')  
+  cpuList=listBuilder(reqCpu) 
+  reqDisk = requests.get('http://localhost:8080/diskdata') 
+  diskList=listBuilder(reqDisk) 
+ 
+  return ramList,diskList ,cpuList 
+ 
+def listBuilder(l): 
+  newList=[] 
+  jsonData=l.json() 
+  items=jsonData.values() 
+  for item in items: 
+    for key in item: 
+      newList.append([key[0],key[1]]) 
+  return newList
