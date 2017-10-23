ef1edf5824dea9156da1bb96c214c99af322669e
comnurz <slbandidas@gmail.com>
2017-10-23 16:24:27 +0300

    Snmp Changed Psutil
    Snmp changed psutil.

--- server.py
+++ server.py
@@ -1,52 +1,37 @@
 from bottle import run, template, get, post, request
-from pysnmp.entity.rfc3413.oneliner import cmdgen
+import psutil
 
-cmdGen = cmdgen.CommandGenerator()
-
-def oid(oids):
-    data=[]
-    errorIndication, errorStatus, errorIndex, varBindTable = cmdGen.nextCmd(
-        cmdgen.CommunityData('public'),
-        cmdgen.UdpTransportTarget(('localhost', 161)),
-        *oids,
-    )
-    if errorIndication:
-        print(errorIndication)
-    else:
-        if errorStatus:
-            print('%s at %s' % (
-                errorStatus.prettyPrint(),
-                errorIndex and varBindTable[-1][int(errorIndex)-1] or '?'
-                )
-            )
-        else:
-            for varBindTableRow in varBindTable:
-                for name, val in varBindTableRow:
-                    list1=[name.prettyPrint(),val.prettyPrint()]
-                    data.append(list1)
-    return data
-
-# server url (example: host:post/data)
+# server url (example: host:post/ramdata)
 @get('/ramdata')
 def ram():
-    oids=['1.3.6.1.4.1.2021.4.5','1.3.6.1.4.1.2021.4.6']
-#               total Ram          ,    total ram used
-    data=oid(oids)
+    data=[]
+    ramdata=psutil.swap_memory()
+    # output = sswap(total=2097147904, used=296128512, free=1801019392, percent=14.1, sin=304193536, sout=677842944)
+    out=["total","used","free","percent","sin","sout"]
+    for i in range (len(ramdata)):
+        data.append([out[i],ramdata[i]])
+
     return {'response': data}
 
 
 @get('/cpudata')
 def cpu():
-    oids=['1.3.6.1.4.1.2021.10.1.3.1','1.3.6.1.4.1.2021.10.1.3.2']
-#           1 minute load               5 minute load
-    data=oid(oids)
+    data=[]
+    cpudata=psutil.cpu_percent(interval=1)
+    # Used cpu percent / output = 4.0 etc.
+    out=["percent"]
+    data.append([out[0],cpudata])
     return {'response': data}
 
 @get('/diskdata')
 def disk():
-    oids=['1.3.6.1.4.1.2021.9.1.8.1','1.3.6.1.4.1.2021.9.1.7.1']
-#           used                        avaible
-    data=oid(oids)
+    data=[]
+    diskdata=psutil.disk_usage('/')
+    # output = sdiskusage(total=21378641920, used=4809781248, free=15482871808, percent=22.5)
+    out=["total","used","free","percent"]
+    for i in range (len(diskdata)):
+        data.append([out[i],diskdata[i]])
+
     return {'response': data}
 
 # make api call
