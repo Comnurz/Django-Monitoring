f10fcc0c50b9c1ced5c5b79d0d2e65385fc7c82a
comnurz <slbandidas@gmail.com>
2017-10-31 15:34:51 +0300

    Bug Fix
    Ready for installation.

--- monitor/views.py
+++ monitor/views.py
@@ -1,33 +1,37 @@
 from django.shortcuts import render
 import requests
 # Create your views here.
-def index(request):  
-  ramValues,diskValues,cpuValues=requestData() 
-  return render(request, 'monitor/index.html', { 
-    'ramValues': ramValues, 
+def index(request):
+  ramValues,diskValues,cpuValues=requestData()
+  return render(request, 'monitor/index.html', {
+    'ramValues': ramValues,
     'diskValues':diskValues,
-    'cpuValues':cpuValues 
- 
-    }) 
- 
-def requestData(): 
-  cpuList= [] 
-  ramList=[] 
-  diskList=[['a',123]] 
-  reqRam = requests.get('http://localhost:8080/ramdata') 
-  ramList=listBuilder(reqRam) 
-  reqCpu = requests.get('http://localhost:8080/cpudata')  
-  cpuList=listBuilder(reqCpu) 
-  reqDisk = requests.get('http://localhost:8080/diskdata') 
-  diskList=listBuilder(reqDisk) 
- 
-  return ramList,diskList ,cpuList 
- 
-def listBuilder(l): 
-  newList=[] 
-  jsonData=l.json() 
-  items=jsonData.values() 
-  for item in items: 
-    for key in item: 
-      newList.append([key[0],key[1]]) 
+    'cpuValues':cpuValues
+
+    })
+
+def requestData():
+  cpuList= []
+  ramList=[]
+  diskList=[]
+  # get ram data
+  resRam = requests.get('http://HOST:PORT/ramdata')
+  ramList=listBuilder(resRam)
+  # get cpu data
+  resCpu = requests.get('http://HOST:PORT/cpudata')
+  cpuList=listBuilder(resCpu)
+  # get disk data
+  resDisk = requests.get('http://HOST:PORT/diskdata')
+  diskList=listBuilder(resDisk)
+
+  return ramList,diskList ,cpuList
+
+# building a list for Google Charts
+def listBuilder(l):
+  newList=[]
+  jsonData=l.json()
+  items=jsonData.values()
+  for item in items:
+    for key in item:
+      newList.append([key[0],key[1]])
   return newList
--- server.py
+++ server.py
@@ -5,8 +5,8 @@ import psutil
 @get('/ramdata')
 def ram():
     data=[]
-    ramdata=psutil.swap_memory()
     # output = sswap(total=2097147904, used=296128512, free=1801019392, percent=14.1, sin=304193536, sout=677842944)
+    ramdata=psutil.swap_memory()
     out=["total","used","free","percent","sin","sout"]
     for i in range (len(ramdata)):
         data.append([out[i],ramdata[i]])
@@ -17,8 +17,8 @@ def ram():
 @get('/cpudata')
 def cpu():
     data=[]
-    cpudata=psutil.cpu_percent(interval=1)
     # Used cpu percent / output = 4.0 etc.
+    cpudata=psutil.cpu_percent(interval=1)
     out=["percent"]
     data.append([out[0],cpudata])
     return {'response': data}
@@ -26,8 +26,8 @@ def cpu():
 @get('/diskdata')
 def disk():
     data=[]
-    diskdata=psutil.disk_usage('/')
     # output = sdiskusage(total=21378641920, used=4809781248, free=15482871808, percent=22.5)
+    diskdata=psutil.disk_usage('/')
     out=["total","used","free","percent"]
     for i in range (len(diskdata)):
         data.append([out[i],diskdata[i]])
@@ -35,4 +35,4 @@ def disk():
     return {'response': data}
 
 # make api call
-run(host='localhost', port=8080)
+run(host='HOST', port=PORT)
