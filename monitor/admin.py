b5d3ef51f984c1ff92dcbf0de8cfb7ff988ebaf9
comnurz <slbandidas@gmail.com>
2017-11-09 15:49:35 +0300

    Update Admin Panel

--- monitor/admin.py
+++ monitor/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Ram,Cpu,Disk,Server
 # Register your models here.
+
+admin.site.register(Ram)
+admin.site.register(Cpu)
+admin.site.register(Disk)
+admin.site.register(Server)
