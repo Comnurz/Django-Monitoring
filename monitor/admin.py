de0b1732907c4b09fe3921e5d3042ee7e46c833c
comnurz <slbandidas@gmail.com>
2017-11-09 17:41:16 +0300

    Update Admin Panel

--- monitor/admin.py
+++ monitor/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
 
-from .models import Ram,Cpu,Disk,Server
+from .models import Ram,Cpu,Disk,Server,Server_User
 # Register your models here.
 
-admin.site.register(Ram)
-admin.site.register(Cpu)
-admin.site.register(Disk)
-admin.site.register(Server)
+class RamAdmin(admin.ModelAdmin):
+    list_display=('total','used','free','percent','sin','sout','date')
+
+class CpuAdmin(admin.ModelAdmin):
+    list_display=('percent','date')
+
+class DiskAdmin(admin.ModelAdmin):
+    list_display=('total','used','free','percent','date')
+
+class ServerAdmin(admin.ModelAdmin):
+    list_display=('server_name','ram_id','cpu_id','disk_id','date')
+
+admin.site.register(Ram,RamAdmin)
+admin.site.register(Cpu,CpuAdmin)
+admin.site.register(Disk,DiskAdmin)
+admin.site.register(Server,ServerAdmin)
+admin.site.register(Server_User)
