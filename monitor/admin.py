e0f59be7cf9d931924f2dcd8d7dbc4d0085117c0
comnurz <slbandidas@gmail.com>
2017-11-10 16:18:27 +0300

    Admin Page Update
    Links include admin page.

--- monitor/admin.py
+++ monitor/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
 from .models import Ram,Cpu,Disk,Server,Server_User
-# Register your models here.
 
+# Display your models details here.
 class RamAdmin(admin.ModelAdmin):
     list_display=('total','used','free','percent','sin','sout','date')
 
@@ -13,8 +13,9 @@ class DiskAdmin(admin.ModelAdmin):
     list_display=('total','used','free','percent','date')
 
 class ServerAdmin(admin.ModelAdmin):
-    list_display=('server_name','ram_id','cpu_id','disk_id','date')
+    list_display=('server_name','ram_link','cpu_link','disk_link','date')
 
+# Register your models here.
 admin.site.register(Ram,RamAdmin)
 admin.site.register(Cpu,CpuAdmin)
 admin.site.register(Disk,DiskAdmin)
--- monitor/models.py
+++ monitor/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
+from django.template.defaultfilters import escape
+from django.core.urlresolvers import reverse
 
 class Ram(models.Model):
     id=models.AutoField(primary_key=True)
@@ -13,7 +17,7 @@ class Ram(models.Model):
            default=timezone.now)
 
     def __str__(self):
-        return str(self.total)
+        return str(self.id)
 
 class Cpu(models.Model):
     id=models.AutoField(primary_key=True)
@@ -22,7 +26,7 @@ class Cpu(models.Model):
            default=timezone.now)
 
     def __str__(self):
-        return str(self.percent)
+        return str(self.id)
 
 class Disk(models.Model):
     id=models.AutoField(primary_key=True)
@@ -34,7 +38,7 @@ class Disk(models.Model):
            default=timezone.now)
 
     def __str__(self):
-        return str(self.total)
+        return str(self.id)
 
 class Server(models.Model):
     id=models.AutoField(primary_key=True)
@@ -45,10 +49,31 @@ class Server(models.Model):
     date = models.DateTimeField(
        default=timezone.now)
 
+    # linked ram_id. Ram_id click event "go to <id> ram update page"
+    def ram_link(self):
+        return '<a href="%s">%s</a>' % (reverse("admin:monitor_ram_change", args=(self.ram_id,)) , escape(self.ram_id))
+
+    ram_link.allow_tags = True
+    ram_link.short_description = "Ram ID"
+    
+    # linked cpu_id. Cpu_id click event "go to <id> cpu update page"
+    def cpu_link(self):
+        return '<a href="%s">%s</a>' % (reverse("admin:monitor_cpu_change", args=(self.cpu_id,)) , escape(self.cpu_id))
+
+    cpu_link.allow_tags = True
+    cpu_link.short_description = "Cpu ID"
+
+    # linked disk_id. Disk_id click event "go to <id> disk update page"
+    def disk_link(self):
+        return '<a href="%s">%s</a>' % (reverse("admin:monitor_disk_change", args=(self.disk_id,)) , escape(self.disk_id))
+
+    disk_link.allow_tags = True
+    disk_link.short_description = "Disk ID"
+
     def __str__(self):
             return self.server_name
 
 class Server_User(models.Model):
     id=models.AutoField(primary_key=True)
     server_id=models.ManyToManyField(Server)
-    user_id=models.ManyToManyField('auth.User')
+    user_id=models.ManyToManyField(User)
