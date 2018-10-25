c62150b00dfd3daf0eeb43813003f7a98abd70a2
caglanurs <caglanurs@skyatlas.com>
2018-10-25 16:18:21 +0300

    typeo

--- monitor/context_processors.py
+++ monitor/context_processors.py
@@ -1,16 +1,19 @@
-from django.contrib.auth.models import User
+from monitor.models import Server, Server_User
 
-from monitor.models import Ram,Cpu,Disk,Server,Server_User
 
 def server(request):
-    current_user=request.user
-    server_userobj=Server_User.objects.filter(user_id=current_user.id).exists() #check server user pairing
+    current_user = request.user
+    # check server user pairing
+    server_userobj = Server_User.objects.filter(
+                    user_id=current_user.id).exists()
     if server_userobj:
-        servers=[]
+        servers = []
         # select * from Server_User where user_id=current_user.id
-        server=Server_User.objects.filter(user_id=current_user.id).values_list('server_id',flat=True)
+        server = Server_User.objects.filter(
+                 user_id=current_user.id).values_list('server_id', flat=True)
         for i in range(len(server)):
-            servers.append(Server.objects.get(id=server[i])) #added server ids in servers array.
-        return {'servers':servers}
+            # added server ids in servers array.
+            servers.append(Server.objects.get(id=server[i]))
+        return {'servers': servers}
     else:
-        return {'servers':"is null"}
+        return {'servers': "is null"}
